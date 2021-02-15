023e1864a61ec49d5cdad50aaac1d4840f6e03c1
mario0000o7 <ducholak1@gmail.com>
2021-02-15 13:39:44 +0100

    Inital commit

---
+++ cmake-build-debug/CMakeFiles/project_Git_train.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_Git_train.dir/main.cpp.o"
+  "project_Git_train"
+  "project_Git_train.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_Git_train.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
